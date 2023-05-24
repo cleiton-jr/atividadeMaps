de39a62c4176fe5328fb59161d23a8313359c62e
CJR <cleiton.junior16@outlook.com>
2023-05-23 21:03:06 -0300

    atividade

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,65 @@
+import { environment } from './../../environments/environment';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GoogleMap } from '@capacitor/google-maps';
+import { Geolocation, Position } from '@capacitor/geolocation';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  @ViewChild('map') mapRef!: ElementRef<HTMLElement>;
+  map!: GoogleMap;
+
+  constructor() {}
+
+  ionViewWillEnter(){
+    this.createMap();
+  }
+
+  async createMap() {
+    this.map = await GoogleMap.create({
+      id: 'my-map',
+      element: this.mapRef.nativeElement,
+      apiKey: environment.mapsKey,
+      config: {
+        center: {
+          lat: -22.489209,
+          lng: -48.546458,
+        },
+        zoom: 1,
+      },
+    });
+    this.buscarPosicao();
+  }
+
+  async buscarPosicao(){
+    const coordinates = await Geolocation.getCurrentPosition();
+
+    console.log('Current position:', coordinates);
+    this.focarMapa(coordinates);
+    };
+
+  focarMapa(coordinates: Position){
+    this.map.setCamera({
+      coordinate:{
+        lat: coordinates.coords.latitude,
+        lng: coordinates.coords.longitude,
+    },
+    zoom: 15,
+    animate: true,
+    });
+    this.adicionarMarcador(coordinates);
+  }
+
+ async adicionarMarcador(coordinates: Position){
+   const markerId = await this.map.addMarker({
+    coordinate: {
+      lat: coordinates.coords.latitude,
+      lng: coordinates.coords.longitude,
+    },
+   })
+ }
+
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  mapsKey: 'AIzaSyD6v5fULYIQf3DgoRdHRxCahyUMBJSYyjc'
+};
